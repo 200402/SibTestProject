fd6272363c71534ab0931d6018b421a28cb1ec65
200402 <r_he_lol@mail.ru>
2022-11-08 05:19:58 +0500

    смена акков

--- SibTestProject/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ SibTestProject/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -9,29 +9,6 @@ export class NavMenuComponent {
   public sign_in_or_nothing = "Войти";
   public sign_up_or_Log_out = "Зарегистрироваться";
 
-  constructor() {
-    if (this.getCookie("Token") != undefined) {
-      this.sign_in_or_nothing = "";
-      this.sign_up_or_Log_out = "Сменить аккаунт";
-    }
-    else {
-      this.sign_in_or_nothing = "Войти";
-      this.sign_up_or_Log_out = "Зарегистрироваться";
-    }
-  }
-
-  public delete_cookie() {
-    document.cookie = 'Token=; Max-Age=0'
-    window.location.href = 'https://localhost:44455';
-  }
-
-  private getCookie(name: string) {
-    let matches = document.cookie.match(new RegExp(
-      "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
-  }
-
   isExpanded = false;
 
   collapse() {
